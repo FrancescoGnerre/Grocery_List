cb0aeac390cc40315e07040a3c49add9bdd2e226
Francesco Gnerre <francesco.gnerre@gmail.com>
2023-09-21 17:32:44 -0700

    add basic DB

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///item.db'
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    item_id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(200), nullable=False)
+    item_status = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Task %r>' % self.item_id
 
 
 @app.route('/')
